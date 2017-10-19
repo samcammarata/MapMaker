1690a0abb015c7d6bf2dcc4ca60a06001294784d
samcammarata <32913373+samcammarata@users.noreply.github.com>
2017-10-18 20:16:39 -0400

    Add files via upload
    Original upload

---
+++ MapMaker/MapMaker/MapMaker.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace MapMaker
+{
+    partial class MapMaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MapArrayGrid = new System.Windows.Forms.DataGridView();
+            this.CurrentMapLabel = new System.Windows.Forms.Label();
+            this.CurrentMapTextBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.Player1SpawnButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Player2SpawnButton = new System.Windows.Forms.Button();
+            this.Player3SpawnButton = new System.Windows.Forms.Button();
+            this.Player4SpawnButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.CreateNewButton = new System.Windows.Forms.Button();
+            this.BackgroundImageTextBox = new System.Windows.Forms.TextBox();
+            this.BackgroundImageLabel = new System.Windows.Forms.Label();
+            this.BackgroundImageUploadButton = new System.Windows.Forms.Button();
+            this.Boss3SpawnButton = new System.Windows.Forms.Button();
+            this.Boss2SpawnButton = new System.Windows.Forms.Button();
+            this.BossSpawnsLabel = new System.Windows.Forms.Label();
+            this.Boss1SpawnButton = new System.Windows.Forms.Button();
+            this.Obstacle3Button = new System.Windows.Forms.Button();
+            this.Obstacle2Button = new System.Windows.Forms.Button();
+            this.ObstaclesLabel = new System.Windows.Forms.Label();
+            this.Obstacle1Button = new System.Windows.Forms.Button();
+            this.Obstacle5Button = new System.Windows.Forms.Button();
+            this.Obstacle4Button = new System.Windows.Forms.Button();
+            this.PreviewButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MapArrayGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MapArrayGrid
+            // 
+            this.MapArrayGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MapArrayGrid.Location = new System.Drawing.Point(12, 12);
+            this.MapArrayGrid.Name = "MapArrayGrid";
+            this.MapArrayGrid.Size = new System.Drawing.Size(527, 319);
+            this.MapArrayGrid.TabIndex = 0;
+            // 
+            // CurrentMapLabel
+            // 
+            this.CurrentMapLabel.AutoSize = true;
+            this.CurrentMapLabel.Location = new System.Drawing.Point(550, 18);
+            this.CurrentMapLabel.Name = "CurrentMapLabel";
+            this.CurrentMapLabel.Size = new System.Drawing.Size(65, 13);
+            this.CurrentMapLabel.TabIndex = 1;
+            this.CurrentMapLabel.Text = "Current Map";
+            // 
+            // CurrentMapTextBox
+            // 
+            this.CurrentMapTextBox.Location = new System.Drawing.Point(548, 34);
+            this.CurrentMapTextBox.Name = "CurrentMapTextBox";
+            this.CurrentMapTextBox.Size = new System.Drawing.Size(195, 20);
+            this.CurrentMapTextBox.TabIndex = 2;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(671, 471);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(74, 35);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // Player1SpawnButton
+            // 
+            this.Player1SpawnButton.Location = new System.Drawing.Point(107, 383);
+            this.Player1SpawnButton.Name = "Player1SpawnButton";
+            this.Player1SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Player1SpawnButton.TabIndex = 6;
+            this.Player1SpawnButton.Text = "P1";
+            this.Player1SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 395);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Player Spawns";
+            // 
+            // Player2SpawnButton
+            // 
+            this.Player2SpawnButton.Location = new System.Drawing.Point(162, 383);
+            this.Player2SpawnButton.Name = "Player2SpawnButton";
+            this.Player2SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Player2SpawnButton.TabIndex = 8;
+            this.Player2SpawnButton.Text = "P2";
+            this.Player2SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // Player3SpawnButton
+            // 
+            this.Player3SpawnButton.Location = new System.Drawing.Point(219, 383);
+            this.Player3SpawnButton.Name = "Player3SpawnButton";
+            this.Player3SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Player3SpawnButton.TabIndex = 9;
+            this.Player3SpawnButton.Text = "P3";
+            this.Player3SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // Player4SpawnButton
+            // 
+            this.Player4SpawnButton.Location = new System.Drawing.Point(281, 383);
+            this.Player4SpawnButton.Name = "Player4SpawnButton";
+            this.Player4SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Player4SpawnButton.TabIndex = 10;
+            this.Player4SpawnButton.Text = "P4";
+            this.Player4SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(591, 471);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(74, 35);
+            this.SaveButton.TabIndex = 11;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // UploadButton
+            // 
+            this.UploadButton.Location = new System.Drawing.Point(431, 469);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(74, 35);
+            this.UploadButton.TabIndex = 12;
+            this.UploadButton.Text = "Upload";
+            this.UploadButton.UseVisualStyleBackColor = true;
+            // 
+            // CreateNewButton
+            // 
+            this.CreateNewButton.Location = new System.Drawing.Point(511, 470);
+            this.CreateNewButton.Name = "CreateNewButton";
+            this.CreateNewButton.Size = new System.Drawing.Size(74, 35);
+            this.CreateNewButton.TabIndex = 13;
+            this.CreateNewButton.Text = "Create New";
+            this.CreateNewButton.UseVisualStyleBackColor = true;
+            // 
+            // BackgroundImageTextBox
+            // 
+            this.BackgroundImageTextBox.Location = new System.Drawing.Point(127, 351);
+            this.BackgroundImageTextBox.Name = "BackgroundImageTextBox";
+            this.BackgroundImageTextBox.Size = new System.Drawing.Size(212, 20);
+            this.BackgroundImageTextBox.TabIndex = 14;
+            // 
+            // BackgroundImageLabel
+            // 
+            this.BackgroundImageLabel.AutoSize = true;
+            this.BackgroundImageLabel.Location = new System.Drawing.Point(24, 354);
+            this.BackgroundImageLabel.Name = "BackgroundImageLabel";
+            this.BackgroundImageLabel.Size = new System.Drawing.Size(97, 13);
+            this.BackgroundImageLabel.TabIndex = 15;
+            this.BackgroundImageLabel.Text = "Background Image";
+            // 
+            // BackgroundImageUploadButton
+            // 
+            this.BackgroundImageUploadButton.Location = new System.Drawing.Point(345, 351);
+            this.BackgroundImageUploadButton.Name = "BackgroundImageUploadButton";
+            this.BackgroundImageUploadButton.Size = new System.Drawing.Size(74, 20);
+            this.BackgroundImageUploadButton.TabIndex = 16;
+            this.BackgroundImageUploadButton.Text = "Upload";
+            this.BackgroundImageUploadButton.UseVisualStyleBackColor = true;
+            // 
+            // Boss3SpawnButton
+            // 
+            this.Boss3SpawnButton.Location = new System.Drawing.Point(219, 426);
+            this.Boss3SpawnButton.Name = "Boss3SpawnButton";
+            this.Boss3SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Boss3SpawnButton.TabIndex = 20;
+            this.Boss3SpawnButton.Text = "B3";
+            this.Boss3SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // Boss2SpawnButton
+            // 
+            this.Boss2SpawnButton.Location = new System.Drawing.Point(162, 426);
+            this.Boss2SpawnButton.Name = "Boss2SpawnButton";
+            this.Boss2SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Boss2SpawnButton.TabIndex = 19;
+            this.Boss2SpawnButton.Text = "B2";
+            this.Boss2SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // BossSpawnsLabel
+            // 
+            this.BossSpawnsLabel.AutoSize = true;
+            this.BossSpawnsLabel.Location = new System.Drawing.Point(24, 438);
+            this.BossSpawnsLabel.Name = "BossSpawnsLabel";
+            this.BossSpawnsLabel.Size = new System.Drawing.Size(71, 13);
+            this.BossSpawnsLabel.TabIndex = 18;
+            this.BossSpawnsLabel.Text = "Boss Spawns";
+            // 
+            // Boss1SpawnButton
+            // 
+            this.Boss1SpawnButton.Location = new System.Drawing.Point(107, 426);
+            this.Boss1SpawnButton.Name = "Boss1SpawnButton";
+            this.Boss1SpawnButton.Size = new System.Drawing.Size(38, 37);
+            this.Boss1SpawnButton.TabIndex = 17;
+            this.Boss1SpawnButton.Text = "B1";
+            this.Boss1SpawnButton.UseVisualStyleBackColor = true;
+            // 
+            // Obstacle3Button
+            // 
+            this.Obstacle3Button.Location = new System.Drawing.Point(219, 469);
+            this.Obstacle3Button.Name = "Obstacle3Button";
+            this.Obstacle3Button.Size = new System.Drawing.Size(38, 37);
+            this.Obstacle3Button.TabIndex = 24;
+            this.Obstacle3Button.Text = "O3";
+            this.Obstacle3Button.UseVisualStyleBackColor = true;
+            // 
+            // Obstacle2Button
+            // 
+            this.Obstacle2Button.Location = new System.Drawing.Point(162, 469);
+            this.Obstacle2Button.Name = "Obstacle2Button";
+            this.Obstacle2Button.Size = new System.Drawing.Size(38, 37);
+            this.Obstacle2Button.TabIndex = 23;
+            this.Obstacle2Button.Text = "O2";
+            this.Obstacle2Button.UseVisualStyleBackColor = true;
+            // 
+            // ObstaclesLabel
+            // 
+            this.ObstaclesLabel.AutoSize = true;
+            this.ObstaclesLabel.Location = new System.Drawing.Point(24, 481);
+            this.ObstaclesLabel.Name = "ObstaclesLabel";
+            this.ObstaclesLabel.Size = new System.Drawing.Size(54, 13);
+            this.ObstaclesLabel.TabIndex = 22;
+            this.ObstaclesLabel.Text = "Obstacles";
+            // 
+            // Obstacle1Button
+            // 
+            this.Obstacle1Button.Location = new System.Drawing.Point(107, 469);
+            this.Obstacle1Button.Name = "Obstacle1Button";
+            this.Obstacle1Button.Size = new System.Drawing.Size(38, 37);
+            this.Obstacle1Button.TabIndex = 21;
+            this.Obstacle1Button.Text = "O1";
+            this.Obstacle1Button.UseVisualStyleBackColor = true;
+            // 
+            // Obstacle5Button
+            // 
+            this.Obstacle5Button.Location = new System.Drawing.Point(338, 469);
+            this.Obstacle5Button.Name = "Obstacle5Button";
+            this.Obstacle5Button.Size = new System.Drawing.Size(38, 37);
+            this.Obstacle5Button.TabIndex = 26;
+            this.Obstacle5Button.Text = "O5";
+            this.Obstacle5Button.UseVisualStyleBackColor = true;
+            // 
+            // Obstacle4Button
+            // 
+            this.Obstacle4Button.Location = new System.Drawing.Point(281, 469);
+            this.Obstacle4Button.Name = "Obstacle4Button";
+            this.Obstacle4Button.Size = new System.Drawing.Size(38, 37);
+            this.Obstacle4Button.TabIndex = 25;
+            this.Obstacle4Button.Text = "O4";
+            this.Obstacle4Button.UseVisualStyleBackColor = true;
+            // 
+            // PreviewButton
+            // 
+            this.PreviewButton.Location = new System.Drawing.Point(669, 60);
+            this.PreviewButton.Name = "PreviewButton";
+            this.PreviewButton.Size = new System.Drawing.Size(74, 35);
+            this.PreviewButton.TabIndex = 27;
+            this.PreviewButton.Text = "Preview";
+            this.PreviewButton.UseVisualStyleBackColor = true;
+            // 
+            // MapMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 520);
+            this.Controls.Add(this.PreviewButton);
+            this.Controls.Add(this.Obstacle5Button);
+            this.Controls.Add(this.Obstacle4Button);
+            this.Controls.Add(this.Obstacle3Button);
+            this.Controls.Add(this.Obstacle2Button);
+            this.Controls.Add(this.ObstaclesLabel);
+            this.Controls.Add(this.Obstacle1Button);
+            this.Controls.Add(this.Boss3SpawnButton);
+            this.Controls.Add(this.Boss2SpawnButton);
+            this.Controls.Add(this.BossSpawnsLabel);
+            this.Controls.Add(this.Boss1SpawnButton);
+            this.Controls.Add(this.BackgroundImageUploadButton);
+            this.Controls.Add(this.BackgroundImageLabel);
+            this.Controls.Add(this.BackgroundImageTextBox);
+            this.Controls.Add(this.CreateNewButton);
+            this.Controls.Add(this.UploadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.Player4SpawnButton);
+            this.Controls.Add(this.Player3SpawnButton);
+            this.Controls.Add(this.Player2SpawnButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Player1SpawnButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CurrentMapTextBox);
+            this.Controls.Add(this.CurrentMapLabel);
+            this.Controls.Add(this.MapArrayGrid);
+            this.Name = "MapMaker";
+            this.Text = "Map Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.MapArrayGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MapArrayGrid;
+        private System.Windows.Forms.Label CurrentMapLabel;
+        private System.Windows.Forms.TextBox CurrentMapTextBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button Player1SpawnButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Player2SpawnButton;
+        private System.Windows.Forms.Button Player3SpawnButton;
+        private System.Windows.Forms.Button Player4SpawnButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button UploadButton;
+        private System.Windows.Forms.Button CreateNewButton;
+        private System.Windows.Forms.TextBox BackgroundImageTextBox;
+        private System.Windows.Forms.Label BackgroundImageLabel;
+        private System.Windows.Forms.Button BackgroundImageUploadButton;
+        private System.Windows.Forms.Button Boss3SpawnButton;
+        private System.Windows.Forms.Button Boss2SpawnButton;
+        private System.Windows.Forms.Label BossSpawnsLabel;
+        private System.Windows.Forms.Button Boss1SpawnButton;
+        private System.Windows.Forms.Button Obstacle3Button;
+        private System.Windows.Forms.Button Obstacle2Button;
+        private System.Windows.Forms.Label ObstaclesLabel;
+        private System.Windows.Forms.Button Obstacle1Button;
+        private System.Windows.Forms.Button Obstacle5Button;
+        private System.Windows.Forms.Button Obstacle4Button;
+        private System.Windows.Forms.Button PreviewButton;
+    }
+}
+
---
+++ MapMaker/MapMaker/MapMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapMaker
+{
+    public partial class MapMaker : Form
+    {
+        public MapMaker()
+        {
+            InitializeComponent();
+        }
+
+        private void PreviewButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
